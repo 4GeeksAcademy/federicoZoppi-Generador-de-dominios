1fd65bdb767ce34fea3d2f1c10950faa02cb04ca
FEDERICO ZOPPI <federicozoppi.ita@gmail.com>
2025-04-29 18:01:39 +0000

    federicoZoppi generador dominios

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let domain = [".com", ".es"]
+
+let allDomain = []
+
+for (let p = 0; p < pronoun.length; p++) {
+  for (let a = 0; a < adj.length; a++) {
+    for (let n = 0; n < noun.length; n++) {
+      for (let f = 0; f < domain.length; f++) {
+        // allDomain = tengo que agregar el dominio en el array 
+        allDomain.push(pronoun[p] + adj[a] + noun[n] + domain[f]);
+      }
+    }
+  }
+}
+window.onload = () => {
+  let domainList = document.getElementById('domain-list');
+
+  domainList.style.listStyle = "none";
+  domainList.style.textAlign = "center";
+
+  domainList.innerHTML = allDomain.map(domain => `<li>${domain}</li>`).join('');
 };
