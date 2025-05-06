e6774730578a4a7eb47c0480a3ff184c70d015eb
FEDERICO ZOPPI <federicozoppi.ita@gmail.com>
2025-05-06 18:15:13 +0000

    [fix] code review

--- src/app.js
+++ src/app.js
@@ -1,28 +1,26 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let pronoun = ['the', 'our'];
-let adj = ['great', 'big'];
-let noun = ['jogger', 'racoon'];
-let domain = [".com", ".es"]
 
-let allDomain = []
+window.onload = () => {
+  let pronouns = ['the', 'our'];
+  let adjectives = ['great', 'big'];
+  let nouns = ['jogger', 'racoon'];
+  let domains = [".com", ".es"]
+  let allDomain = []
 
-for (let p = 0; p < pronoun.length; p++) {
-  for (let a = 0; a < adj.length; a++) {
-    for (let n = 0; n < noun.length; n++) {
-      for (let f = 0; f < domain.length; f++) {
-        // allDomain = tengo que agregar el dominio en el array 
-        allDomain.push(pronoun[p] + adj[a] + noun[n] + domain[f]);
+  for (let pronoun of pronouns) {
+    for (let adjective of adjectives) {
+      for (let noun of nouns) {
+        for (let domain of domains) {
+          allDomain.push(`${pronoun}${adjective}${noun}${domain}`);
+        }
       }
     }
   }
-}
-window.onload = () => {
+  
   let domainList = document.getElementById('domain-list');
 
   domainList.style.listStyle = "none";
